2b13310f9550b5d6f0f00fdc2a070de11c08b637
Abdullah Ahmad <bdodevel@gmail.com>
2021-03-30 23:34:41 +0200

    added material icons

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -10,5 +10,8 @@ export default function Head() {
     <meta charSet="UTF-8" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"></link>
   </NextHead>
 }
