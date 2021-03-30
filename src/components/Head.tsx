017674326c59673273b3727783c28e7c39a338b1
Abdullah Ahmad <bdodevel@gmail.com>
2021-03-30 20:45:42 +0200

    added meta tags

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -6,5 +6,9 @@ export default function Head() {
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet"></link>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet"></link>
+
+    <meta charSet="UTF-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   </NextHead>
 }
