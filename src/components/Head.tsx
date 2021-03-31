e541f30b5a4bed1bbfb4073b9ec063d90d195fc2
Abdullah Ahmad <bdodevel@gmail.com>
2021-03-31 17:56:58 +0200

    added ubuntu mono font

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,8 +4,9 @@ export default function Head() {
   return <NextHead>
     <title>DevChallenges Input Component</title>
     <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet"></link>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet"></link>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap" rel="stylesheet" />
 
     <meta charSet="UTF-8" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
